95ea565adc034ea195fe4bd7b9ca41391e521cf6
Saleck3 <aledagonale@gmail.com>
2025-06-10 11:55:27 +0200

    Removed double call

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,14 +6,13 @@ use App\UseCases\Product\GetAllProductsUseCase;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 final class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'app_products')]
-    public function index(GetAllProductsUseCase $getAllProductsUseCase, SerializerInterface $serializer): JsonResponse
+    public function index(GetAllProductsUseCase $getAllProductsUseCase): JsonResponse
     {
         $products = $getAllProductsUseCase->getAll();
-        return $this->json($serializer->serialize($products, 'json'));
+        return $this->json($products);
     }
 }
