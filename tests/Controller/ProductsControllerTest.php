63c922f5fd9c161e5f8a7cf5b6a00f59a3a2a1c2
Saleck3 <aledagonale@gmail.com>
2025-06-10 15:56:57 +0200

    Added ProductsController tests

--- tests/Controller/ProductsControllerTest.php
+++ tests/Controller/ProductsControllerTest.php
@@ -2,15 +2,38 @@
 
 namespace App\Tests\Controller;
 
+use App\Domain\Product\ProductRepositoryInterface;
+use App\Tests\Factory\Product\ProductFactory;
+use App\UseCases\Product\GetAllProductsUseCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 final class ProductsControllerTest extends WebTestCase
 {
+    use ResetDatabase;
+
     public function testIndex(): void
     {
         $client = static::createClient();
         $client->request('GET', '/products');
 
         self::assertResponseIsSuccessful();
+    }
+
+    public function testCanRetrieveProducts(): void
+    {
+        $expected = 2;
+        //Given expected products
+        for ($i = 0; $i < $expected; $i++) {
+            ProductFactory::createOne();
+        }
+
+        //When retrieving products
+        $productRepository = $this->getContainer()->get(ProductRepositoryInterface::class);
+        $productsUseCase = new GetAllProductsUseCase($productRepository);
+        $allProducts = $productsUseCase->getAll();
+
+        //Then products quantity is the same
+        self::assertEquals($expected, count($allProducts));
     }
 }
