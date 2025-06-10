0f94d57a7cd914b7eb13be016ff675b2ec70a1ed
Saleck3 <aledagonale@gmail.com>
2025-06-10 15:56:20 +0200

    Added database reset for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,15 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+passthru("php bin/console --env=test doctrine:database:drop --force");
+passthru("php bin/console --env=test doctrine:database:create");
+passthru("php bin/console --env=test doctrine:schema:create");
