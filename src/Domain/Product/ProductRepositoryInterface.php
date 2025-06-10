92d0158217121b7ebe22a4f50abad733fd2bbb63
Saleck3 <aledagonale@gmail.com>
2025-06-10 18:49:01 +0200

    Added GetProductByIdUseCase

--- src/Domain/Product/ProductRepositoryInterface.php
+++ src/Domain/Product/ProductRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace App\Domain\Product;
 interface ProductRepositoryInterface
 {
     public function findAll(): array;
+    public function findOneById(int $id): ?Product;
 }
---
+++ src/UseCases/Product/ProductNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\UseCases\Product;
+
+class ProductNotFoundException extends \Exception
+{
+
+}
